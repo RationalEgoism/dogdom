16e2976598e291d3d53e32c04c4e388304b961ab
Stanislav Talanov <rationalegoismforwork@gmail.com>
2022-02-07 22:21:38 +0300

    Add private policy & signing configs

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         // This is the theme of your application.
@@ -24,7 +25,13 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const Center(child: Text('Text')),
+      home: const Center(
+          child: Text(
+        'Text',
+        style: TextStyle(
+          color: Colors.red,
+        ),
+      )),
     );
   }
 }

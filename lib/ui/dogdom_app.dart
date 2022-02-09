ae303b6664bf425aa63093fd1984994796426e94
RationalEgoism <43000755+RationalEgoism@users.noreply.github.com>
2022-02-09 21:12:41 +0300

    #10 add eng\ru l10n (#11)

--- lib/ui/dogdom_app.dart
+++ lib/ui/dogdom_app.dart
@@ -1,5 +1,6 @@
 import 'package:dogdom/ui/login_screen/login_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class DogdomApp extends StatelessWidget {
   const DogdomApp({Key? key}) : super(key: key);
@@ -8,11 +9,13 @@ class DogdomApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Dogdom',
+      onGenerateTitle: (context) => AppLocalizations.of(context)!.appTitle,
       theme: ThemeData(
         fontFamily: 'Gilroy',
         primarySwatch: Colors.blue,
       ),
+      localizationsDelegates: AppLocalizations.localizationsDelegates,
+      supportedLocales: AppLocalizations.supportedLocales,
       home: LoginScreen(),
     );
   }

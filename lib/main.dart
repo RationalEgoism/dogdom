910591e8171e1a392189bdb5f67919128f357328
RationalEgoism <43000755+RationalEgoism@users.noreply.github.com>
2022-02-10 03:46:45 +0300

    #15 add splash screen for light & dark mode with 1 sec delay (#16)
    * #15 add flutter_native_splash dependency
    
    * #15 add splash screen for light & dark mode with 1 sec delay

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
 import 'package:dogdom/ui/dogdom_app.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 void main() {
+  FlutterNativeSplash.removeAfter(initializeFunction);
   runApp(const DogdomApp());
+}
+
+void initializeFunction(BuildContext context) async {
+  Future.delayed(Duration(seconds: 1));
 }

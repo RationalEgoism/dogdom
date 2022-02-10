e356d0368ff5885214278918f466e40639326ad3
Stanislav Talanov <rationalegoismforwork@gmail.com>
2022-02-10 04:49:32 +0300

    #17 add navigation to main screen (wip)

--- lib/theme/app_icons.dart
+++ lib/theme/app_icons.dart
@@ -1,4 +1,8 @@
 class AppIcons {
+  // Common
+  static const wip = 'assets/image/wip.png';
+
+  // Login
   static const logo = 'assets/image/logo.svg';
   static const bgLoginScreen = 'assets/image/bg_login_screen.png';
 }
---
+++ lib/ui/main_screen/main_screen.dart
@@ -0,0 +1,28 @@
+import 'package:dogdom/theme/app_icons.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+class MainScreen extends StatelessWidget {
+  static String id = "MainScreen";
+
+  const MainScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Image.asset(
+          AppIcons.wip,
+          height: 200,
+          width: 200,
+        ),
+        Text(
+          AppLocalizations.of(context)!.wip,
+          textAlign: TextAlign.center,
+        ),
+      ],
+    );
+  }
+}

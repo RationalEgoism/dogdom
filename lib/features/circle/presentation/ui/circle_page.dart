24c3775928981d9e1d37348c3b900be1a42913a7
Stanislav Talanov <rationalegoismforwork@gmail.com>
2022-02-17 11:07:04 +0300

    #50 add tabs pages

---
+++ lib/features/circle/presentation/ui/circle_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class CirclePage extends StatelessWidget {
+  const CirclePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Circle'),
+    );
+  }
+}

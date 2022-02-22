c27d32a084d1f1c034d5ce8540df1e8b5c23d4e6
Stanislav Talanov <rationalegoismforwork@gmail.com>
2022-02-22 11:59:21 +0300

    #71 add icons

--- lib/features/home_select/presentation/ui/home_select_page.dart
+++ lib/features/home_select/presentation/ui/home_select_page.dart
@@ -174,6 +174,9 @@ class HomeSelectPage extends StatelessWidget {
                           ),
                         ),
                       ),
+                      Row(
+                        children: [],
+                      ),
                     ],
                   ),
                 ),

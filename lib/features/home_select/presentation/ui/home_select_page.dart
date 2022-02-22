9534cb59a6115ccae65f03ab00c2e9e6b36f3b7e
Stanislav Talanov <rationalegoismforwork@gmail.com>
2022-02-22 10:32:14 +0300

    #71 wip news widget, title

--- lib/features/home_select/presentation/ui/home_select_page.dart
+++ lib/features/home_select/presentation/ui/home_select_page.dart
@@ -93,56 +93,76 @@ class HomeSelectPage extends StatelessWidget {
                 child: Container(
                   color: Colors.greenAccent,
                   child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Row(
-                        children: [
-                          CircleAvatar(
-                            radius: 15.0,
-                            backgroundImage: AssetImage(
-                              Assets.imageMock.homeSelectAvatar1.path,
-                            ),
-                          ),
-                          Expanded(
-                            child: Padding(
-                              padding: const EdgeInsets.symmetric(
-                                horizontal: 8.0,
-                                vertical: 4.5,
+                      Container(
+                        color: Colors.indigo,
+                        child: Row(
+                          children: [
+                            CircleAvatar(
+                              radius: 15.0,
+                              backgroundImage: AssetImage(
+                                Assets.imageMock.homeSelectAvatar1.path,
                               ),
-                              child: Text(
-                                'Mirabelle Swift',
-                                style: TextStyle(
-                                  color: Colors.black,
+                            ),
+                            Expanded(
+                              child: Padding(
+                                padding: const EdgeInsets.symmetric(
+                                  horizontal: 8.0,
+                                  vertical: 4.5,
+                                ),
+                                child: Text(
+                                  'Mirabelle Swift',
+                                  style: TextStyle(
+                                    color: Colors.black,
+                                  ),
                                 ),
                               ),
                             ),
-                          ),
-                          SizedBox.fromSize(
-                            size: Size(
-                              70.0,
-                              28.0,
-                            ),
-                            child: TextButton(
-                              onPressed: () {
-                                //
-                              },
-                              child: Text(
-                                'Follow',
-                                style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 12.0,
+                            SizedBox.fromSize(
+                              size: Size(
+                                70.0,
+                                28.0,
+                              ),
+                              child: TextButton(
+                                onPressed: () {
+                                  //
+                                },
+                                child: Text(
+                                  'Follow',
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 12.0,
+                                  ),
+                                ),
+                                style: TextButton.styleFrom(
+                                  padding: EdgeInsets.zero,
+                                  backgroundColor: ColorName.red,
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(4),
+                                  ),
                                 ),
                               ),
-                              style: TextButton.styleFrom(
-                                padding: EdgeInsets.zero,
-                                backgroundColor: ColorName.red,
-                                shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(4),
-                                ),
-                              ),
                             ),
-                          ),
-                        ],
+                          ],
+                        ),
                       ),
+                      Container(
+                        color: Colors.red,
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(
+                            vertical: 10.0,
+                          ),
+                          child: Text(
+                            'Adwords Keyword Research For Beginners.',
+                            textAlign: TextAlign.start,
+                            style: TextStyle(
+                              color: ColorName.black70,
+                              fontSize: 15.0,
+                            ),
+                          ),
+                        ),
+                      )
                     ],
                   ),
                 ),

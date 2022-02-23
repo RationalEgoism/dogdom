5c67f6fc81f277d1bb99aeaa2f15add1cde34d7d
Stanislav Talanov <rationalegoismforwork@gmail.com>
2022-02-23 05:50:01 +0300

    #71 removed extra containers

--- lib/app/theme/widgets/news_post.dart
+++ lib/app/theme/widgets/news_post.dart
@@ -37,146 +37,129 @@ class NewsPost extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.greenAccent,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Container(
-            color: Colors.indigo,
-            child: Row(
-              children: [
-                CircleAvatar(
-                  radius: 15.0,
-                  backgroundImage: AssetImage(
-                    avatarImgPath,
-                  ),
-                ),
-                Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(
-                      horizontal: 8.0,
-                      vertical: 4.5,
-                    ),
-                    child: Text(
-                      userName,
-                      style: TextStyle(
-                        color: Colors.black,
-                      ),
-                    ),
-                  ),
-                ),
-                SizedBox.fromSize(
-                  size: Size(
-                    70.0,
-                    28.0,
-                  ),
-                  child: TextButton(
-                    onPressed: onFollowTap,
-                    child: Text(
-                      LocaleKeys.common_follow.tr(),
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 12.0,
-                      ),
-                    ),
-                    style: TextButton.styleFrom(
-                      padding: EdgeInsets.zero,
-                      backgroundColor: ColorName.red,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(4),
-                      ),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
-          Container(
-            color: Colors.red,
-            child: Padding(
-              padding: const EdgeInsets.symmetric(
-                vertical: 10.0,
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Row(
+          children: [
+            CircleAvatar(
+              radius: 15.0,
+              backgroundImage: AssetImage(
+                avatarImgPath,
               ),
-              child: Text(
-                title,
-                textAlign: TextAlign.start,
-                style: TextStyle(
-                  color: ColorName.black70,
-                  fontSize: 15.0,
+            ),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 8.0,
+                  vertical: 4.5,
+                ),
+                child: Text(
+                  userName,
+                  style: TextStyle(
+                    color: Colors.black,
+                  ),
                 ),
               ),
             ),
-          ),
-          Container(
-            color: Colors.yellow,
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(
-                8.0,
+            SizedBox.fromSize(
+              size: Size(
+                70.0,
+                28.0,
               ),
-              child: Image.asset(
-                postImgPath,
-              ),
-            ),
-          ),
-          Container(
-            child: Padding(
-              padding: const EdgeInsets.symmetric(
-                vertical: 10.0,
-              ),
-              child: Row(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(
-                      right: 24.0,
-                    ),
-                    child: SocialIcon(
-                      imgPath: Assets.image.like.path,
-                      text: likeCount.toString(),
-                      onTap: onLikeTap,
-                    ),
+              child: TextButton(
+                onPressed: onFollowTap,
+                child: Text(
+                  LocaleKeys.common_follow.tr(),
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 12.0,
                   ),
-                  Padding(
-                    padding: const EdgeInsets.only(
-                      right: 24.0,
-                    ),
-                    child: SocialIcon(
-                      imgPath: Assets.image.comments.path,
-                      text: commentCount.toString(),
-                      onTap: onCommentTap,
-                    ),
+                ),
+                style: TextButton.styleFrom(
+                  padding: EdgeInsets.zero,
+                  backgroundColor: ColorName.red,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(4),
                   ),
-                  Padding(
-                    padding: const EdgeInsets.only(
-                      right: 24.0,
-                    ),
-                    child: SocialIcon(
-                      imgPath: Assets.image.share.path,
-                      text: shareCount.toString(),
-                      onTap: onShareTap,
-                    ),
-                  ),
-                  Spacer(),
-                  Container(
-                    color: Colors.blue,
-                    child: Material(
-                      color: Colors.transparent,
-                      child: InkWell(
-                        splashColor: ColorName.red,
-                        onTap: onMoreTap,
-                        child: SvgPicture.asset(
-                          Assets.image.more.path,
-                          color: ColorName.black40,
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
+                ),
               ),
             ),
+          ],
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(
+            vertical: 10.0,
           ),
-        ],
-      ),
+          child: Text(
+            title,
+            textAlign: TextAlign.start,
+            style: TextStyle(
+              color: ColorName.black70,
+              fontSize: 15.0,
+            ),
+          ),
+        ),
+        ClipRRect(
+          borderRadius: BorderRadius.circular(
+            8.0,
+          ),
+          child: Image.asset(
+            postImgPath,
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(
+            vertical: 10.0,
+          ),
+          child: Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(
+                  right: 24.0,
+                ),
+                child: SocialIcon(
+                  imgPath: Assets.image.like.path,
+                  text: likeCount.toString(),
+                  onTap: onLikeTap,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(
+                  right: 24.0,
+                ),
+                child: SocialIcon(
+                  imgPath: Assets.image.comments.path,
+                  text: commentCount.toString(),
+                  onTap: onCommentTap,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(
+                  right: 24.0,
+                ),
+                child: SocialIcon(
+                  imgPath: Assets.image.share.path,
+                  text: shareCount.toString(),
+                  onTap: onShareTap,
+                ),
+              ),
+              Spacer(),
+              Material(
+                color: Colors.transparent,
+                child: InkWell(
+                  splashColor: ColorName.red,
+                  onTap: onMoreTap,
+                  child: SvgPicture.asset(
+                    Assets.image.more.path,
+                    color: ColorName.black40,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
